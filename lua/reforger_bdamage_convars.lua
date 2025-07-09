14621b0f60f6cd8b94d35306b2b67dc7acf8ac83
RareIridium77 <219307991+RareIridium77@users.noreply.github.com>
2025-07-10 03:49:29 +0500

    Changed Convar name to keep.gibs

--- lua/reforger_bdamage_convars.lua
+++ lua/reforger_bdamage_convars.lua
@@ -1,3 +1,3 @@
 if SERVER and Reforger then
-    Reforger.CreateConvar("keep_gibs", "0", "Keep vehicle gibs in your map", 0, 1)
+    Reforger.CreateConvar("keep.gibs", "0", "Keep vehicle gibs in your map (server)", 0, 1)
 end
